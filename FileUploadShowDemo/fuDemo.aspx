8eb01be4d6d20cf3bbc90d55821538f7df6a90a1
Suraj Kushvaha <surajkushvaha04@gmail.com>
2022-02-24 19:51:12 +0530

    file upload demo

---
+++ FileUploadShowDemo/fuDemo.aspx
@@ -0,0 +1,81 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="fuDemo.aspx.cs" Inherits="fuDemo" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style="display:flex; flex-direction:column;justify-content:center;align-items:center">
+        <asp:Label ID="lblFileDemo" runat="server"></asp:Label><br />
+        <asp:FileUpload ID="fuFileDemo" runat="server" />
+        <div style="flex-direction:row">
+            <asp:Button ID="btnUpload" runat="server" Text="Upload" OnClick="btnUpload_Click" />
+            <asp:Button ID="btnDelete" runat="server" Text="Delete" OnClick="btnDelete_Click" />
+        </div>      
+    </div>
+<hr />
+        <%--<section >
+        <h2>Contact</h2>
+        <div id="lblMsgDiv" runat="server" visible="false" >
+            <asp:Label ID="lblContactPhotoMsg" runat="server"
+                EnableViewState="False" Visible="False"></asp:Label>
+            <asp:Label ID="lblErrMsg" runat="server"
+                EnableViewState="False" Visible="False"></asp:Label>
+        </div>
+
+        <div >
+            <div >
+                <asp:Label ID="lblContactPhotoName" runat="server" Text="ContactPhoto Name" ></asp:Label>
+            </div>
+            <div>
+                <asp:TextBox ID="txtContactPhotoName" CssClass="form-control" runat="server"></asp:TextBox>
+                
+            </div>
+        </div>
+        <div >
+            <div>
+                <asp:Label ID="lblContactPhotoFile" runat="server" Text="ContactPhoto Image" ></asp:Label>
+            </div>
+            <div >
+                <asp:FileUpload ID="fuContactPhoto" runat="server" />
+                <asp:RegularExpressionValidator ID="rexp" runat="server" ControlToValidate="fuContactPhoto"
+     ErrorMessage="Only .gif, .jpg, .png, .tiff and .jpeg"
+     ValidationExpression="(.*\.([Gg][Ii][Ff])|.*\.([Jj][Pp][Gg])|.*\.([Bb][Mm][Pp])|.*\.([pP][nN][gG])|.*\.([tT][iI][iI][fF])$)"></asp:RegularExpressionValidator>
+             </div>
+        </div>
+        <div >
+
+            <asp:Button ID="btnAdd" runat="server" Text="Add"
+                OnClick="btnAdd_Click" />
+        </div>
+    </section> 
+
+        <hr />--%>
+        <%--<asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" 
+            DataSourceID="SqlDataSource1" OnRowCreated="GridView1_RowCreated">
+            <Columns>
+                <asp:BoundField DataField="ContactName" HeaderText="ContactName" 
+                    SortExpression="ContactName" />
+                <asp:BoundField DataField="ContactPhotoPath" HeaderText="ContactPhotoPath" 
+                    SortExpression="ContactPhotoPath" />
+                <asp:TemplateField>
+                    <ItemTemplate>
+                        <asp:Image runat="server" ImageUrl='<%#  Eval("ContactPhotoPath") %>' Height="50px"/>                        
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <%-- <asp:BoundField DataField="ContactPhotoPath" HeaderText="ContactPhotoPath" 
+                    SortExpression="ContactPhotoPath" />--%>
+          <%--  </Columns>
+        </asp:GridView>
+        <asp:Label runat="server" ID="lblQWERT"></asp:Label>
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" 
+            ConnectionString="<%$ ConnectionStrings:MultiuserAddressBookV2ConnectionString2 %>" 
+            SelectCommand="SELECT [ContactName], [ContactPhotoPath] FROM [ContactPhoto]">
+        </asp:SqlDataSource>--%>
+    </form>
+</body>
+</html>
---
+++ FileUploadShowDemo/fuDemo.aspx.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class fuDemo : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnUpload_Click(object sender, EventArgs e)
+    {
+        if (fuFileDemo.HasFile)
+        {
+            lblFileDemo.Text = " file is Selected : Name (" + fuFileDemo.FileName.ToString() + ")<br/>";
+            lblFileDemo.Text += "file type : " + fuFileDemo.PostedFile.ContentType + "<br/>";
+            lblFileDemo.Text += "file Extension" + System.IO.Path.GetExtension(fuFileDemo.FileName) + "<br/>";
+            int fileSize = fuFileDemo.PostedFile.ContentLength;
+            String MBOrKB = "";
+            float fileSIzeInMb = fileSize / (1024*1024);
+            MBOrKB = "MB";
+            if (fileSIzeInMb <= 0) {
+                fileSIzeInMb = fileSize / 1024;
+                MBOrKB = "KB";
+
+            }
+            lblFileDemo.Text += "<br/>  file Size :" + fileSIzeInMb + MBOrKB;
+            String folderPath = "~/userContent/" ;
+            String AbsolutePath = Server.MapPath(folderPath);
+            lblFileDemo.Text += "<br/>  file will be uploaded on the location :" + AbsolutePath;
+
+            if (!Directory.Exists(AbsolutePath))
+            {
+                Directory.CreateDirectory(AbsolutePath);
+            }
+            fuFileDemo.SaveAs(AbsolutePath + fuFileDemo.FileName.ToString());
+
+        }
+        else
+        {
+            lblFileDemo.Text = "Please Select a file";
+            return;
+        }
+    }
+    protected void btnDelete_Click(object sender, EventArgs e)
+    {
+        String AbsolutePath = Server.MapPath("~/userContent1/waves.png");
+        FileInfo file = new FileInfo(AbsolutePath);
+        if (file.Exists)
+        {
+            file.Delete();
+            lblFileDemo.Text = "Deleted Successfully";
+
+        }
+        else
+        {
+            lblFileDemo.Text = "File Not Availabe";
+        }
+        //if (Directory.Exists(Server.MapPath("~/userContent1")))
+        //{
+        //    lblFileDemo.Text = "1";
+        //    Directory.Delete(Server.MapPath("~/userContent1"));
+
+        //}
+        //[PR_ContactPhoto_Insert]
+        //else{
+        //    lblFileDemo.Text ="Folder not Availabel";
+        //}
+       
+    }
+    //protected void btnAdd_Click(object sender, EventArgs e)
+    //{
+    //    SqlConnection objConn = new SqlConnection(ConfigurationManager.ConnectionStrings["AddressBookConnectionString"].ConnectionString);
+    //    String fileName = "";
+    //    String strErrorMsg = "";
+
+    //    if (txtContactPhotoName.Text.Trim() == "")
+    //    {
+    //        strErrorMsg += "- Enter Contact name.<br/>";
+
+    //    }
+
+    //    if (fuContactPhoto.HasFile)
+    //    {
+    //        fileName =  "~/userContent/" + fuContactPhoto.FileName.ToString();
+    //        String folderPath = "~/userContent/";
+    //        String AbsolutePath = Server.MapPath(folderPath);
+
+    //        if (!Directory.Exists(AbsolutePath))
+    //        {
+    //            Directory.CreateDirectory(AbsolutePath);
+    //        }
+    //        fuContactPhoto.SaveAs(AbsolutePath + fuContactPhoto.FileName.ToString());
+
+    //    }
+    //    else
+    //    {
+    //        strErrorMsg = "Please Select a file";
+    //        return;
+    //    }
+    //    if (strErrorMsg != "")
+    //    {
+    //        lblContactPhotoMsg.Visible = true;
+    //        lblMsgDiv.Visible = true;
+    //        lblContactPhotoMsg.Text = strErrorMsg;
+    //        return;
+    //    }
+        
+       
+      
+    //    try
+    //    {
+    //        if (objConn.State != ConnectionState.Open)
+    //            objConn.Open();
+
+    //        SqlCommand objCmd = objConn.CreateCommand();
+    //        objCmd.CommandType = CommandType.StoredProcedure;
+
+
+    //        objCmd.Parameters.AddWithValue("@ContactName", txtContactPhotoName.Text);
+    //        objCmd.Parameters.AddWithValue("@ContactPhotoPath", fileName);
+
+
+    //        objCmd.CommandText = "[dbo].[PR_ContactPhoto_Insert]";
+    //            objCmd.ExecuteNonQuery();
+    //            lblContactPhotoMsg.Visible = true;
+    //            lblMsgDiv.Visible = true;
+    //            lblContactPhotoMsg.Text = "Data Inserted Successfully";
+    //            txtContactPhotoName.Text = "";
+    //            txtContactPhotoName.Text = "";
+
+    //            txtContactPhotoName.Focus();
+            
+
+
+    //        if (objConn.State == ConnectionState.Open)
+    //            objConn.Close();
+
+    //    }
+    //    catch (Exception exc)
+    //    {
+    //        lblErrMsg.Visible = true;
+    //        lblMsgDiv.Visible = true;
+    //        lblErrMsg.Text = exc.Message;
+
+    //    }
+    //    finally
+    //    {
+    //        if (objConn.State == ConnectionState.Open)
+    //            objConn.Close();
+
+    //    }
+
+    //}
+
+    //protected void gvContactPhotoList_Load(object sender, EventArgs e)
+    //{
+
+    //    foreach (GridViewRow row in gvContactPhotoList.Rows)
+    //    {
+    //        string lblFileSize = "";
+
+    //        String fileName = row.Cells[1].ToString().Trim();
+    //        lblFileSize = fileName;
+
+    //    }
+    //}
+  
+}
